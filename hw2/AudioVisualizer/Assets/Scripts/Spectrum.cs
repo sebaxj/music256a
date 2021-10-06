fd267b0671e8b656f441f602389d6f5be8c2e2bb
Sebastian James <sebaxj@stanford.edu>
2021-10-06 16:11:57 -0700

    updated .gitignore to NOT ignore scripts, but everything else in AudioVisualizer.

---
+++ hw2/AudioVisualizer/Assets/Scripts/Spectrum.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//  Why is the magnitude spectrum half of the time domain
+//  
+
+//-----------------------------------------------------------------------------
+// name: Spectrum.cs
+// desc: set up and draw the spectrum
+//-----------------------------------------------------------------------------
+public class Spectrum : MonoBehaviour
+{
+    // prefab reference
+    public GameObject the_pfCube;
+    // array of cubes
+    public GameObject[] the_cubes = new GameObject[512];
+
+    // spectrum history matrix
+    public float[,] history = new float[32, 512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // x, y, z
+        float x = -512, y = 0, z = 0;
+        // increment
+        float xIncrement = the_pfCube.transform.localScale.x * 2;
+        // place the cubes initially
+        for ( int i = 0; i < the_cubes.Length; i++ )
+        {
+            // instantiate
+            GameObject go = Instantiate(the_pfCube);
+            // color material
+            go.GetComponent<Renderer>().material.SetColor("_BaseColor", new Color(.5f, .5f, 1));
+            // transform it
+            go.transform.position = new Vector3(x, y, z);
+            // increment x
+            x += xIncrement;
+            // scale it to be 2x wider
+            go.transform.localScale = new Vector3(2, 0, 1);
+            // give it name
+            go.name = "bin" + i;
+            // set this as a child of this Spectrum
+            go.transform.parent = this.transform;
+            // put into array
+            the_cubes[i] = go;
+        }
+
+        // position this
+        this.transform.position = new Vector3(this.transform.position.x, -100, this.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // local refernce to the spectrum
+        float[] spectrum = ChunityAudioInput.the_spectrum;
+
+        // deal with spectrum history
+
+        for( int i = 0; i < the_cubes.Length; i++ )
+        {
+            float y = 600 * Mathf.Sqrt(spectrum[i]);
+            the_cubes[i].transform.localScale =
+                new Vector3(the_cubes[i].transform.localScale.x,
+                y,
+                the_cubes[i].transform.localScale.z);
+            the_cubes[i].transform.localPosition =
+                new Vector3(the_cubes[i].transform.localPosition.x,
+                y/2,
+                the_cubes[i].transform.localPosition.z);
+        }
+    }
+}
---
+++ hw2/AudioVisualizer/Assets/Scripts/Waveform.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//-----------------------------------------------------------------------------
+// name: Waveform.cs
+// desc: set up and draw the audio waveform
+//-----------------------------------------------------------------------------
+public class Waveform : MonoBehaviour
+{
+    // prefab reference
+    public GameObject the_pfCube;
+    // array of game objects
+    public GameObject[] the_cubes = new GameObject[1024];
+    // controllable scale
+    public float MY_SCALE = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = -512, y = 0, z = 0;
+        // calculate
+        float xIncrement = the_pfCube.transform.localScale.x;
+
+        for( int i = 0; i < the_cubes.Length; i++ )
+        {
+            // instantiate a prefab game object
+            GameObject go = Instantiate(the_pfCube);
+            // color material
+            go.GetComponent<Renderer>().material.SetColor("_BaseColor", new Color(.5f, 1, .5f));
+            // default position
+            go.transform.position = new Vector3(x, y, z);
+            // increment the x position
+            x += xIncrement;
+            // give a name!
+            go.name = "cube" + i;
+            // set a child of this waveform
+            go.transform.parent = this.transform;
+            // put into array
+            the_cubes[i] = go;
+        }
+
+        // position this
+        this.transform.position = new Vector3(this.transform.position.x, 100, this.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // local reference to the time domain waveform
+        float[] wf = ChunityAudioInput.the_waveform;
+
+        // position the cubes
+        for( int i = 0; i < the_cubes.Length; i++ )
+        {
+            the_cubes[i].transform.localPosition =
+                new Vector3(the_cubes[i].transform.localPosition.x,
+                            MY_SCALE * wf[i],
+                            the_cubes[i].transform.localPosition.z);
+        }
+    }
+}
