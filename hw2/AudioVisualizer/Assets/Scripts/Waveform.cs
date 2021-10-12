f0aa8f2ceaa90f44fef27373e083bcb1fcb8eef6
Sebastian James <sebaxj@stanford.edu>
2021-10-11 17:46:15 -0700

    added images to assets

--- hw2/AudioVisualizer/Assets/Scripts/Waveform.cs
+++ hw2/AudioVisualizer/Assets/Scripts/Waveform.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// localPosition is the position of the transform relative 
+// to the parent transform (Waveform (parent) -> cube (child))
+
+// position is the position of te transform in world space
 
 //-----------------------------------------------------------------------------
 // name: Waveform.cs
@@ -9,17 +13,21 @@ using UnityEngine;
 //-----------------------------------------------------------------------------
 public class Waveform : MonoBehaviour
 {
-    // constant to control
-    private const int WAVEFORM_MIDDLE_ANCHOR = 265;
+    // constant to control where the waveform is anchored in the middle
+    // of the screen
+    private const int WAVEFORM_MIDDLE_ANCHOR = 245;
 
+    // constant to for the number of cubes (same as number of samples in time domain)
+    // to store in the array
     private const int NUM_CUBES = 1024;
 
     // prefab reference
     public GameObject the_pfCube;
     // array of game objects
     public GameObject[] the_cubes = new GameObject[NUM_CUBES];
-    // controllable scale
-    public float MY_SCALE = 200; // what does this do?
+
+    // controllable scale of the y-axis movement "amplification" of cubes
+    public float MY_SCALE = 400;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +37,8 @@ public class Waveform : MonoBehaviour
         // y axis controls "height" of objects from top to bottom
         float x = -512, y = 0, z = 0; 
 
-        // calculate
+        // the x increment is calculated as a function of the local scale of the cube
+        // to that they are placed exactly side by side
         float xIncrement = the_pfCube.transform.localScale.x;
 
         for( int i = 0; i < the_cubes.Length; i++ )
@@ -41,7 +50,7 @@ public class Waveform : MonoBehaviour
                 go.GetComponent<Renderer>().material.SetColor("_BaseColor", new Color(.5f, 1, .5f));
                 // default position
                 go.transform.position = new Vector3(x, y, z);
-
+                //
                 // give a name!
                 go.name = "cube" + i;
                 // set a child of this waveform
@@ -53,7 +62,7 @@ public class Waveform : MonoBehaviour
             x += xIncrement;
         }
 
-        // position this
+        // position this ('this' refers to the waveform)
         this.transform.position = new Vector3(this.transform.position.x, 0, this.transform.position.z);
     }
 
@@ -61,7 +70,8 @@ public class Waveform : MonoBehaviour
     void Update()
     {
         // local reference to the time domain waveform
-        float[] wf = ChunityAudioInput.the_waveform;
+        // contains 1024 floating point numbers with the magnitude of waveform
+        float[] wf = ChunityAudioInput.the_waveform; 
 
         // position the cubes
         for( int i = 0; i < the_cubes.Length; i++ )
@@ -73,3 +83,5 @@ public class Waveform : MonoBehaviour
         }
     }
 }
+
+
